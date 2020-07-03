caa5795c49dc950dd16a0099a998648f07f13500
Chris Joakim <christopher.joakim@gmail.com>
2020-07-03 12:05:06 -0400

    .gitignore and venv.ps1 on win 10

--- py/venv.ps1
+++ py/venv.ps1
@@ -3,21 +3,15 @@
 # Requires Python 3; version 3.7 or higher recommended.
 # Chris Joakim, Microsoft, 2020/07/03
 
-echo 'upgrading pip ...'
-python -m pip install --upgrade pip
-
-echo 'creating new venv ...'
-python -m venv .
-
-echo 'activating new venv ...'
-.\Scripts\Activate.ps1
+echo 'displaying python and pip versions; python 3.7+ expected ...'
 python --version
 pip --version
 
-rm .\requirements.txt
+echo 'deleting Scripts directory ...'
+del .\Scripts\
 
-echo 'pip-compile requirements.in ...'
-pip-compile --output-file .\requirements.txt .\requirements.in
+echo 'creating new venv ...'
+python -m venv .
 
 echo 'pip install requirements.txt ...'
 pip install -r .\requirements.txt

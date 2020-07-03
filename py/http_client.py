e07597afede175b3232f0da3ca03488c48b3b1f3
Chris Joakim <christopher.joakim@gmail.com>
2020-07-03 07:47:06 -0400

    enhanced http_client.py

--- py/http_client.py
+++ py/http_client.py
@@ -9,50 +9,75 @@ from time import gmtime, strftime
 
 from faker import Faker
 from faker.providers import address
+from faker.providers import barcode
 from faker.providers import internet
 
 import arrow
 import requests
 
 # Python HTTP Client for the Azure Logic App.
-# Chris Joakim, Microsoft, 2020/06/29
+# Chris Joakim, Microsoft, 2020/07/03
 #
 # Usage:
 # $ python http_client.py invoke_http_logic_app 
 
-STATES = 'VA,TN,AL,NC,SC'.split(',')
+US_STATES = 'AK,AL,AR,AZ,CA,CO,CT,DC,DE,FL,GA,HI,IA,ID,IL,IN,KS,KY,LA,MA,MD,ME,MI,MN,MO,MS,MT,NC,ND,NE,NH,NJ,NM,NV,NY,OH,OK,OR,PA,RI,SC,SD,TN,TX,UT,VA,VT,WA,WI,WV,WY'.split(',')
 
+# The Faker library is used to create randomized values
 fake = Faker()
 fake.add_provider(address)
+fake.add_provider(barcode)
 fake.add_provider(internet)
 
 def invoke_http_logic_app():
     # Generate and HTTP Post a randomized JSON document to the Azure Logic App
-    # A generated document looks like this:
-    # {'pk': 'E', 'epoch': 1593443622, 'x': 'X-70948', 'y': 'Y-6834'}
-
-    states = load_us_states()
-    state_abbreviations = sorted(states.keys())
-
     url = os.environ['AZURE_LOGICAPP_URL']
     headers = {'Content-Type': 'application/json'}
+    body = generate_message_body()
+    print(json.dumps(body, indent=2))
+    execute_post(url, headers, body)
+
+def generate_message_body():
+    device = fake.ean(length=13)
+    r1 = int(random.random() * 100)
+    r2 = int(random.random() * 100)
+    r3 = int(random.random() * 100)
+    normal_temp = 60 + int(random.random() * 20)
+    normal_cv = int(random.random() * 10)
     body = {}
     body['id'] = str(uuid.uuid4())
-    body['pk'] = random.choice(state_abbreviations)
+    body['pk'] = device
+    body['device_id'] = device
     body['epoch'] = arrow.utcnow().timestamp
-    body['doctype'] = 'logic_app_post'
-
-    # https://github.com/joke2k/faker/blob/master/tests/providers/test_address.py
+    body['temperature'] = normal_temp  # temperature sensor
+    body['cv_threat'] = normal_cv      # computer-vision sensor
     body['name'] = fake.name()
     body['city'] = fake.city()
     body['address'] = '{} {} {}'.format(
         fake.building_number(), fake.street_name(), fake.street_suffix())
-    body['state'] = fake.state()
-    body['message'] = fake.text()
+    body['state'] = random.choice(US_STATES)
+    body['doctype'] = 'home_security_system_heartbeat'
+    body['randoms'] = '{} {} {}'.format(r1, r2, r3)
 
-    body['x']  = 'X-{}'.format(str(int(random.random() * 100000)))
-    body['y']  = 'Y-{}'.format(str(int(random.random() * 100000)))
-    execute_post(url, headers, body)
+    # 10% of these generated messages will indicate an anomaly
+    if r1 < 10:
+        if r2 < 50:
+            # temperature anomalies
+            if r3 < 50:
+                body['temperature'] = 500 + int(random.random() * 100)  # house on fire!
+                body['cv_threat'] = normal_cv
+            else:
+                body['temperature'] = 200 + int(random.random() * 50)  # something is burning
+                body['cv_threat'] = normal_cv 
+        else:
+            # cv anomalies
+            if r3 < 50:
+                body['temperature'] = normal_temp
+                body['cv_threat'] = 90 + int(random.random() * 10)  # intruder is in the house!
+            else:   
+                body['temperature'] = normal_temp
+                body['cv_threat'] = 30 + int(random.random() * 50)  # someone is outside
+    return body
 
 def execute_post(url, headers, body):
     jstr = json.dumps(body)
@@ -80,19 +105,46 @@ def explore_faker():
         states[s] = n
     print(json.dumps(states, indent=2, sort_keys=True))
 
+def debug_messages():
+    for n in range(100):
+        body = generate_message_body()
+        t  = body['temperature']
+        cv = body['cv_threat']
+        t_msg, cv_msg = '', ''
+        if t >= 200:
+            t_msg = 'warm'
+        if t >= 500:
+            t_msg = 'fire'
+        if cv >= 30:
+            cv_msg = 'outside'
+        if cv >= 90:
+            cv_msg = 'intruder'
+
+        print('{} {} | {} {} | {}'.format(t, t_msg, cv, cv_msg, body['randoms']))
+
+def ad_hoc():
+    states = load_us_states()
+    state_abbreviations = sorted(states.keys())
+    print(states)
+    print(state_abbreviations)
+    print(','.join(state_abbreviations))
+
+
 if __name__ == "__main__":
 
     if len(sys.argv) > 1:
-        func  = sys.argv[1].lower()
-        if func == 'invoke_http_logic_app':
-            invoke_http_logic_app()
-        elif func == 'explore_faker':
+        func = sys.argv[1].lower()
+        if func == 'explore_faker':
             explore_faker()
+        elif func == 'debug_messages':
+            debug_messages()
+        elif func == 'ad_hoc':
+            ad_hoc()
         elif func == 'load_us_states':
             d = load_us_states()
             print(d)
             print(sorted(d.keys()))
         else:
-            print('undefined func: {}'.format(func))
+            invoke_http_logic_app()
     else:
         print('specify a command line function')
